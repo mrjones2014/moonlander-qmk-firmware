f1bae4032a2cb042e6c7794be6ef20f388c774fd
Mat Jones <mat@mjones.network>
2025-10-18 16:16:06 -0400

    chore(setup): Seed oryx branch with downloaded oryx config

--- src/rules.mk
+++ src/rules.mk
@@ -1,8 +1,5 @@
-CONSOLE_ENABLE = no
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+LINK_TIME_OPTIMIZATION_ENABLE = yes
 COMMAND_ENABLE = no
-AUTO_SHIFT_ENABLE = yes
-ORYX_ENABLE = yes
-RGB_MATRIX_CUSTOM_KB = yes
-TAP_DANCE_ENABLE = yes
-SPACE_CADET_ENABLE = no
-OS_DETECTION_ENABLE = yes
+RGBLIGHT_ENABLE = no
