26362d79409b82084725cf396b5d2049f5e14223
Mat Jones <mat@mjones.network>
2025-10-23 21:13:46 -0400

    chore: add doc comments

--- src/config.h
+++ src/config.h
@@ -7,6 +7,8 @@
 #define LEADER_NO_TIMEOUT
 #define LEADER_PER_KEY_TIMING
 #define LEADER_TIMEOUT 250
+// Zelda puzzle solved song
+// Composed using https://getreuer.info/posts/keyboards/qmk-song-player
 #define ZELDA_PUZZLE                                                           \
   Q__NOTE(_G5), Q__NOTE(_FS5), Q__NOTE(_DS5), Q__NOTE(_A4), Q__NOTE(_GS4),     \
       Q__NOTE(_E5), Q__NOTE(_GS5), HD_NOTE(_C6),
