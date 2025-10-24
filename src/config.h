4afd7c24e6be82c1ed78fa177e6a7b7a4265121b
Mat Jones <mat@mjones.network>
2025-10-23 21:06:41 -0400

    feat: play zelda puzzle song on startup

--- src/config.h
+++ src/config.h
@@ -7,3 +7,7 @@
 #define LEADER_NO_TIMEOUT
 #define LEADER_PER_KEY_TIMING
 #define LEADER_TIMEOUT 250
+#define ZELDA_PUZZLE                                                           \
+  Q__NOTE(_G5), Q__NOTE(_FS5), Q__NOTE(_DS5), Q__NOTE(_A4), Q__NOTE(_GS4),     \
+      Q__NOTE(_E5), Q__NOTE(_GS5), HD_NOTE(_C6),
+#define STARTUP_SONG SONG(ZELDA_PUZZLE)
