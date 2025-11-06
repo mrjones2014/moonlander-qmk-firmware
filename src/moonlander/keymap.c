44f19c1d3aa450f7cb4a3008327eca41ab276762
Mat Jones <mat@mjones.network>
2025-11-06 16:00:44 -0500

    feat(moonlander): Tweak layout slightly

--- src/moonlander/keymap.c
+++ src/moonlander/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // base
     [0] = LAYOUT_moonlander(
         KC_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_PRINT_SCREEN,                                                           KC_F5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, THIN_ARROW,                                                                      FAT_ARROW, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_UNDERSCORE,                                                                      KC_MINUS, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
         KC_LEFT_SHIFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_HYPR,                                                         KC_MEH, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOTE,
         KC_GRAVE, MT(MOD_LCTL, KC_Z), KC_X, KC_C, KC_V, KC_B,                                                                   KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_EQUAL,
         KC_LEFT_CTRL, KC_TRANSPARENT, KC_LEFT_ALT, KC_LEFT, KC_RIGHT,    KC_LEFT_ALT,                      LGUI(KC_SPACE),           KC_DOWN, KC_UP, KC_LBRC, KC_RBRC, TO(2),
